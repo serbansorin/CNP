6c92598fbabfeab4b0ce88092fc885b60f0f4028
Serban Sorin Alex <31596288+serbansorin@users.noreply.github.com>
2020-10-12 11:49:46 +0300

    none

--- index.php
+++ index.php
@@ -1,20 +1,77 @@
 <?php
 // VALIDARE CNP
 
+
 function validareCNP($CNP)
 {
     // CNP must have 13 characters
-    if (strlen($CNP) != 13) {
+    if (strlen($CNP) != 13 && !is_numeric($CNP)) {
         return false;
     }
     //transform numarul in array
     $cnp = str_split($CNP);
+
+    #SEXUL
+
+    $sex = $cnp[0];
+    $sex == 9 ? ($sex = 'strain') : ($sex % 2 ? $sex = 'masculin' : $sex = 'feminin'); // schimb valoarea $sex din numar
+
+    #Anul Nasterii
+
+    $an = ($cnp[1] * 10) + $cnp[2]; // anul in care s-au nascut
+    switch ($cnp[0]) {
+        case 1:
+        case 2:{
+                $an += 1900;
+            }
+            break; // cetateni romani nascuti intre 1 ian 1900 si 31 dec 1999
+        case 3:
+        case 4:{
+                $an += 1800;
+            }
+            break; // cetateni romani nascuti intre 1 ian 1800 si 31 dec 1899
+        case 5:
+        case 6:{
+                $an += 2000;
+            }
+            break; // cetateni romani nascuti intre 1 ian 2000 si 31 dec 2099
+        case 7:
+        case 8:
+        case 9:{
+                $an += 1900;
+            }
+            break; // rezidenti si Cetateni Straini
+
+    }
+
+    $ziuaNasterii = $cnp[4][5] . '/' . $cnp[6][7] . '/' . $an; //ziua de nastere
+
+    ####
+    
+    
+    ####JUDETUL####
+
+
+
+
+    ####
+
+    ####Validare Numarul NNN####
+
+
+
+    
+    ####
+
+
+    ####ALGORITMUL DE VALIDARE CIFRA CONTROL CU TABEL DE CONTROL####
     
 
     $tabelControl = array(2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9); // numarul de control
     $cifraControl = 0; // declar cifra de control ca variabila
 
-    // All characters must be numeric
+
+
     for ($i = 0; $i < 13; $i++) {
         if (!is_numeric($cnp[$i])) {
             return false;
@@ -29,37 +86,8 @@ function validareCNP($CNP)
     if ($cifraControl == 10) {
         $cifraControl = 1;
     }
-    //declar $sex
-    $sex = $cnp[0];
-    $sex == 9 ? ($sex = 'strain') : ($sex % 2 ? $sex = 'masculin' : $sex = 'feminin'); // schimb valoarea $sex din numar
 
-    
-    $an = ($cnp[1] * 10) + $cnp[2]; // anul in care s-au nascut
-    switch ($cnp[0]) {
-        case 1:
-        case 2: {
-                $an += 1900;
-            }
-            break; // cetateni romani nascuti intre 1 ian 1900 si 31 dec 1999
-        case 3:
-        case 4: {
-                $an += 1800;
-            }
-            break; // cetateni romani nascuti intre 1 ian 1800 si 31 dec 1899
-        case 5:
-        case 6: {
-                $an += 2000;
-            }
-            break; // cetateni romani nascuti intre 1 ian 2000 si 31 dec 2099
-        case 7:
-        case 8:
-        case 9: {
-                $an += 1900;
-            }
-            break; // rezidenti si Cetateni Straini
+    return ($cnp[12] === $cifraControl); // Asta ar fii validarea initiala
 
-    }
-    $ziuaNasterii= $cnp[4][5].'/'.$cnp[6][7].'/'.$an ; //ziua de nastere
-
-    ($an > 1800 && $an < 2099 && $cnp[12] == $cifraControl) ? true : false; // am validat cnp
+    #####Final######
 }
